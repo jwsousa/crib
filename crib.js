929055eb8bba3285e3e1238cd392f068b3ee7289
Michael Cusack <mpcusack@mpcusack.com>
2012-11-16 19:12:09 -0500

    store by sockets

--- crib.js
+++ crib.js
@@ -196,7 +196,7 @@ exports.Game = function(io){
   this.requestCrib = function(socketId){
     var game = this;
     this.requestCards(socketId, 2, function (cards) {
-      game.addCrib(this.role[socketId], cards);
+      game.addCrib(game.role[socketId], cards);
     });
   }
   this.requestCard = function(socketId){
