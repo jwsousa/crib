891934a097586c98fa79e9f77d57083dd204b82c
Michael Cusack <mpcusack@mpcusack.com>
2012-11-16 19:24:54 -0500

    card tostring

--- crib.js
+++ crib.js
@@ -250,7 +250,7 @@ exports.cardFromDeckIndex = function(index){
   }
   var card = {'suit': suit, 'face': face, 'score': score};
   card.toString = function(){
-    return 'Card[' + this.suit + this.face + ']';
+    return 'Card[' + suit + face + ']';
   }
 }
 
