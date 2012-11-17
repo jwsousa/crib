0f4d99ac39edf8a7734cad0a379b11d0800cc88a
Michael Cusack <mpcusack@mpcusack.com>
2012-11-16 20:52:19 -0500

    new hand

--- crib.js
+++ crib.js
@@ -214,12 +214,9 @@ exports.Game = function(io){
     var socket = this.sockets[socketId];
     socket.emit('set cards',{'section': 'crib', 'cards': []});
     socket.emit('set count',{'count': -1});
-    socket.emit('set unflipped', {'section': 'flip',
-                                  'number': 1});
-    socket.emit('set unflipped', {'section': 'hand',
-                                  'number': this.cards[this.roles[socketId]].length});
-    socket.emit('set unflipped', {'section': 'otherhand',
-                                  'number': this.cards[this.roles[this.opponent[socketId]]].length});
+    socket.emit('set unflipped', {'section': 'flip', 'number': 1});
+    socket.emit('set unflipped', {'section': 'hand', 'number': 6});
+    socket.emit('set unflipped', {'section': 'otherhand', 'number': 6});
   }
   this.setCribUnflipped = function(){
     this.emitToRoom('set unflipped', {'section': 'crib', 'number': 4});
